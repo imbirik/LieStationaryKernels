a671a7a6a34d52b687ddd305d63237bad6cd5724
imbirik <zarydim@gmail.com>
2022-06-29 02:59:21 +0300

    Grassmanians! (and a little refactoring of Stiefel)

--- src/spaces/grassmanian.py
+++ src/spaces/grassmanian.py
@@ -1,6 +1,7 @@
 import torch
 import numpy as np
 from src.spaces.so import SO, SOLBEigenspace
+from src.spaces.stiefel import _SO
 from src.space import HomogeneousSpace
 from geomstats.geometry.stiefel import Stiefel as Stiefel_
 from src.utils import hook_content_formula
@@ -11,48 +12,38 @@ from src.space import LBEigenspaceWithSum, LieGroupCharacter, AveragedLieGroupCh
 dtype = torch.float64
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-
-class _SO:
+class _SO_SO:
     """Helper class for sampling"""
-    def __init__(self, n):
-        self.n = n
-        self.dim = n*(n-1)//2
+    def __init__(self, n, m):
+        self.n, self.m = n, m
+        self.SOn = _SO(n)
+        self.SOm = _SO(m)
+        self.dim = n*(n-1)//2 + m*(m-1)//2 -1
 
     def rand(self, n=1):
-        h = torch.randn((n, self.n, self.n), device=device, dtype=dtype)
-        q, r = torch.linalg.qr(h)
-        r_diag_sign = torch.sign(torch.diagonal(r, dim1=-2, dim2=-1))
-        q *= r_diag_sign[:, None]
-        q_det_sign = torch.sign(torch.det(q))
-        q[:, :, 0] *= q_det_sign[:, None]
-        return q
+        h_u = self.SOn.rand(n)
+        h_d = self.SOm.rand(n)
+        zeros = torch.zeros((n, self.n, self.m), device=device, dtype=dtype)
+        zerosT = torch.transpose(zeros, dim0=-1, dim1=-2)
+        l, r = torch.cat((h_u, zerosT), dim=1), torch.cat((zeros, h_d), dim=-2)
+        res = torch.cat((l, r), dim=-1)
+        return res
 
-
-class Stiefel(HomogeneousSpace, Stiefel_):
-    """Class for stiefel manifold represented as SO(n)/SO(m)"""
-    """Elements represented as orthonormal frames"""
+class Grassmanian(HomogeneousSpace, Stiefel_):
+    """Class for stiefel manifold represented as SO(n)/SO(m)xSO(n-m)"""
+    """Elements represented as orthonormal frames of size m i.e. matrices nxm"""
     def __init__(self, n, m, order=10, average_order=1000):
         assert n > m, "n should be greater than m"
         self.n, self.m = n, m
         self.n_m = n - m
         G = SO(n, order=order)
-        H = _SO(self.n_m)
+        H = _SO_SO(self.m, self.n_m)
         HomogeneousSpace.__init__(self, G=G, H=H, average_order=average_order)
         Stiefel_.__init__(self, n, m)
         self.id = torch.zeros((self.n, self.m), device=device, dtype=dtype).fill_diagonal_(1.0)
 
     def H_to_G(self, h):
-        """Embed ortogonal matrix Q in the following way
-            I 0
-            0 Q
-        """
-        #h -- [b, n-m, n-m]
-        zeros = torch.zeros((h.size()[0], self.m, self.n_m), device=device, dtype=dtype)
-        zerosT = torch.transpose(zeros, dim0=-1, dim1=-2)
-        eye = torch.eye(self.m, device=device, dtype=dtype).view(1, self.m, self.m).repeat(h.size()[0], 1, 1)
-        l, r = torch.cat((eye, zerosT), dim=1), torch.cat((zeros, h), dim=-2)
-        res = torch.cat((l, r), dim=-1)
-        return res
+        return h
 
     def M_to_G(self, x):
         g, r = torch.linalg.qr(x, mode='complete')
@@ -75,14 +66,8 @@ class Stiefel(HomogeneousSpace, Stiefel_):
         raise NotImplementedError
 
     def close_to_id(self, x):
-        x_ = x[:, :, :self.m].reshape(x.shape[:-2] + (-1,))
-        id_ = self.id.reshape((-1,))
-        return torch.all(torch.isclose(x_, id_[None, ...]), dim=-1)
+        x_ = x[:, :self.m, :self.m].reshape(x.shape[:-2] + (-1,))
+        return torch.all(torch.isclose(x_, torch.zeros_like(x_)), dim=-1)
 
     def compute_inv_dimension(self, signature):
-        m_ = min(self.m, self.n_m)
-        if m_ < self.G.rank and signature[m_] > 0:
-                return 0
-        signature_abs = tuple(abs(x) for x in signature)
-        inv_dimension = hook_content_formula(signature_abs, m_)
-        return inv_dimension
+        return 1
