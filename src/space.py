Fixed torus_representative calculation for SO(2n), small tweaks in tests

src/space.py
@@ -244,7 +244,7 @@ class LieGroupCharacter(torch.nn.Module, ABC):
 
     def evaluate(self, x):
         """Calculates the character value at an element of the group"""
-        gammas = self.representation.manifold.torus_embed(x)
+        gammas = self.representation.manifold.torus_representative(x)
         return self.chi(gammas)
 
     def forward(self, gammas):

src/spaces/so.py
@@ -14,7 +14,6 @@ from pathlib import Path
 
 dtype = torch.float64
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
-pi = 2*torch.acos(torch.zeros(1)).item()
 
 
 class SO(CompactLieGroup):
@@ -39,7 +38,6 @@ class SO(CompactLieGroup):
         self.id = torch.eye(self.n, device=device, dtype=dtype)
         CompactLieGroup.__init__(self, order=order)
 
-
     def difference(self, x, y):
         return x @ y.T
 
@@ -48,7 +46,7 @@ class SO(CompactLieGroup):
         diff = torch.bmm(x, self.inv(y))
         torus_diff = self.torus_representative(diff)
         log_torus_diff = torch.arccos(torus_diff.real)
-        dist = math.sqrt(2) * torch.minimum(log_torus_diff, 2 * pi - log_torus_diff)
+        dist = math.sqrt(2) * torch.minimum(log_torus_diff, 2 * math.pi - log_torus_diff)
         dist = torch.norm(dist, dim=1)
         return dist
 
@@ -56,7 +54,7 @@ class SO(CompactLieGroup):
         """For n points x_i and m points y_j computed dist(x_i,y_j)"""
         x_y_ = self.pairwise_embed(x, y)
         log_x_y_ = torch.arccos(x_y_.real)
-        dist = math.sqrt(2)*torch.minimum(log_x_y_, 2*pi-log_x_y_)
+        dist = math.sqrt(2) * torch.minimum(log_x_y_, 2 * math.pi - log_x_y_)
         dist = torch.norm(dist, dim=1).reshape(x.shape[0], y.shape[0])
         return dist
 
@@ -114,14 +112,17 @@ class SO(CompactLieGroup):
             eigvals, eigvecs = torch.linalg.eig(x)
             sorted_ind = torch.sort(torch.view_as_real(eigvals), dim=-2).indices[..., 0]
             eigvals = torch.gather(eigvals, dim=-1, index=sorted_ind)
-            sorted_ind_vecs = torch.tile(sorted_ind.view(-1, self.n, 1), (1, 1, self.n))
-            eigvecs = torch.gather(eigvecs, dim=1, index=sorted_ind_vecs)
+            eigvecs = torch.gather(eigvecs, dim=-1, index=sorted_ind.unsqueeze(-2).broadcast_to(eigvecs.shape))
             # c is a matrix transforming x into its canonical form (with 2x2 blocks)
             c = torch.zeros_like(eigvecs)
-            c[..., ::2] = eigvecs[..., ::2].real
-            c[..., 1::2] = eigvecs[..., ::2].imag
-            c *= math.sqrt(2)
-            eigvals[..., 0] = torch.pow(eigvals[..., 0], torch.det(c))
+            c[..., ::2] = eigvecs[..., ::2] + eigvecs[..., 1::2]
+            c[..., 1::2] = (eigvecs[..., ::2] - eigvecs[..., 1::2])
+            # eigenvectors calculated by LAPACK are either real or purely imaginary, make everything real
+            # WARNING: might depend on the implementation of the eigendecomposition!
+            c = c.real + c.imag
+            # normalize s.t. det(c)≈±1, probably unnecessary
+            c /= math.sqrt(2)
+            torch.pow(eigvals[..., 0], torch.det(c).sgn(), out=eigvals[..., 0])
             gamma = eigvals[..., ::2]
             return gamma
 
@@ -168,7 +169,6 @@ class SOLBEigenspace(LBEigenspaceWithSum):
 
 class SOCharacter(LieGroupCharacter):
     """Representation character for special orthogonal group"""
-    # @staticmethod
 
     @staticmethod
     def xi0(qs, gamma):
@@ -288,8 +288,6 @@ class SOCharacterDenominatorFree(LieGroupCharacter):
 
 
 class SO3Character(LieGroupCharacter):
-    @staticmethod
-
     def chi(self, x):
         l = self.representation.index[0]
         gamma = self.representation.manifold.torus_representative(x)

tests/grassmannian.py
@@ -1,37 +1,37 @@
 import unittest
 from parameterized import parameterized_class
 import torch
-#from functorch import vmap
-from torch.autograd.functional import _vmap as vmap
 import numpy as np
 from parameterized import parameterized_class
 from src.spaces.grassmannian import Grassmannian, OrientedGrassmannian
 from src.spectral_kernel import EigenbasisSumKernel, EigenbasisKernel, RandomPhaseKernel
 from src.spectral_measure import SqExpSpectralMeasure, MaternSpectralMeasure
 from src.prior_approximation import RandomPhaseApproximation
-from src.utils import cartesian_prod
 
 dtype = torch.float64
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
+torch.set_printoptions(precision=5, sci_mode=False, linewidth=120, edgeitems=5)
 
-
-# @parameterized_class([
-#     {'space': Grassmannian, 'n': 3, 'm': 1, 'order': 10, 'dtype': torch.double},
-#     {'space': Grassmannian, 'n': 4, 'm': 2, 'order': 10, 'dtype': torch.double},
-#     {'space': Grassmannian, 'n': 5, 'm': 2, 'order': 10, 'dtype': torch.double},
-#     {'space': Grassmannian, 'n': 5, 'm': 1, 'order': 10, 'dtype': torch.double},
-#     {'space': OrientedGrassmannian, 'n': 3, 'm': 1, 'order': 10, 'dtype': torch.double},
-#     {'space': OrientedGrassmannian, 'n': 4, 'm': 2, 'order': 10, 'dtype': torch.double},
-#     {'space': OrientedGrassmannian, 'n': 5, 'm': 2, 'order': 10, 'dtype': torch.double},
-#     {'space': OrientedGrassmannian, 'n': 5, 'm': 1, 'order': 10, 'dtype': torch.double},
-# ], class_name_func=lambda cls, num, params_dict: f'Test_{params_dict["space"].__name__}.'
-#                                                  f'{params_dict["n"]},{params_dict["m"]}.{params_dict["order"]}')
-class TestGrassmanian(unittest.TestCase):
+@parameterized_class([
+    {'space': Grassmannian, 'n': 3, 'm': 1, 'order': 10, 'dtype': torch.double},
+    {'space': Grassmannian, 'n': 4, 'm': 1, 'order': 10, 'dtype': torch.double},
+    {'space': Grassmannian, 'n': 4, 'm': 2, 'order': 10, 'dtype': torch.double},
+    {'space': Grassmannian, 'n': 5, 'm': 1, 'order': 10, 'dtype': torch.double},
+    {'space': Grassmannian, 'n': 5, 'm': 2, 'order': 10, 'dtype': torch.double},
+    {'space': Grassmannian, 'n': 6, 'm': 1, 'order': 10, 'dtype': torch.double},
+    {'space': Grassmannian, 'n': 6, 'm': 2, 'order': 10, 'dtype': torch.double},
+    {'space': Grassmannian, 'n': 6, 'm': 3, 'order': 10, 'dtype': torch.double},
+    {'space': OrientedGrassmannian, 'n': 4, 'm': 2, 'order': 10, 'dtype': torch.double},
+    {'space': OrientedGrassmannian, 'n': 5, 'm': 2, 'order': 10, 'dtype': torch.double},
+    {'space': OrientedGrassmannian, 'n': 6, 'm': 2, 'order': 10, 'dtype': torch.double},
+    {'space': OrientedGrassmannian, 'n': 6, 'm': 3, 'order': 10, 'dtype': torch.double},
+], class_name_func=lambda cls, num, params_dict: f'Test_{params_dict["space"].__name__}.'
+                                                 f'{params_dict["n"]},{params_dict["m"]}.{params_dict["order"]}')
+class TestGrassmannian(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.n, self.m = 3, 1
-        self.order, self.average_order = 10, 1000
-        self.space = Grassmannian(self.n, self.m, self.order, self.average_order)
+        self.average_order = 1000
+        self.space = self.space(self.n, self.m, self.order, self.average_order)
         #self.space = Grassmannian(n=self.n, m=self.m, order=self.order, average_order=self.average_order)
 
         self.lengthscale, self.nu = 1.0, 5.0
@@ -49,18 +49,18 @@ class TestGrassmanian(unittest.TestCase):
         self.assertTrue(torch.allclose(x_norm, torch.ones_like(x_norm)))
 
     def test_symmetry(self):
-        if self.m == 1 and isinstance(self.space, Grassmannian):
-            cov = self.func_kernel(self.x, -self.x)
-            print(cov)
-            diag_cov = torch.diagonal(cov)
-            self.assertTrue(torch.allclose(diag_cov, torch.ones_like(diag_cov), atol=5e-2))
-
+        if not (self.m == 1 and isinstance(self.space, Grassmannian)):
+            self.skipTest('Only for Gr(_,1).')
+        cov = self.func_kernel(self.x, -self.x)
+        # print(cov)
+        diag_cov = torch.diagonal(cov)
+        self.assertTrue(torch.allclose(diag_cov, torch.ones_like(diag_cov), atol=5e-2))
 
     def test_prior(self) -> None:
         cov_func = self.func_kernel(self.x, self.x)
         cov_prior = self.sampler_kernel(self.x, self.x).evaluate()
-        print(cov_func)
-        print(cov_prior)
+        # print(cov_func)
+        # print(cov_prior)
         print(cov_prior-cov_func)
         self.assertTrue(torch.allclose(cov_prior, cov_func, atol=5e-2))
 
@@ -82,10 +82,11 @@ class TestGrassmanian(unittest.TestCase):
             cov1 = (f(x_yinv)/dim_sq_f).real
             embed_x, embed_y = self.embed(f, x), self.embed(f, y)
             cov2 = ((embed_x @ torch.conj(embed_y.T))/dim_sq_f).real
-            print(cov1)
-            print(cov2)
+            # print(cov1)
+            # print(cov2)
+            print(cov1 - cov2)
             #self.assertTrue(torch.allclose(cov1, cov2, atol=5e-2, rtol=5e-2))
-            print('passed')
+            # print('passed')
 
 
 if __name__ == '__main__':

tests/lie_groups.py
@@ -23,6 +23,7 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 torch.set_printoptions(precision=2, sci_mode=False, linewidth=160, edgeitems=15)
 
+
 # Parametrized test, produces test classes called Test_Group.dim.order, for example, Test_SO.3.10 or Test_SU.2.5
 @parameterized_class([
     {'group': SO, 'dim': 3, 'order': 20, 'dtype': torch.double},
@@ -84,7 +85,7 @@ class TestCompactLieGroups(unittest.TestCase):
             j, irrep2 = b
             chi1, chi2 = irrep1.basis_sum, irrep2.basis_sum
             scalar_products[i, j] = torch.mean(torch.conj(chi1.chi(gammas)) * chi2.chi(gammas))
-        # print(torch.max(torch.abs(scalar_products - torch.eye(num_irreps, dtype=torch.cdouble))).item())
+        print(torch.max(torch.abs(scalar_products - torch.eye(num_irreps, dtype=torch.cdouble))).item())
         self.assertTrue(torch.allclose(scalar_products, torch.eye(num_irreps, dtype=torch.cdouble), atol=5e-2))
 
     @unittest.skip('Very long, not part of the standard testing routine.')
@@ -124,8 +125,7 @@ class TestCompactLieGroups(unittest.TestCase):
         phase, weight = self.sampler.phases[0], self.sampler.weights[0]  # [num_phase, ...], [num_phase]
         x_phase_inv = self.group.pairwise_diff(x, phase)
         eigen_embedding = f(x_phase_inv).view(x.size()[0], phase.size()[0])
-        eigen_embedding = eigen_embedding / np.sqrt(
-            self.sampler.phase_order)
+        eigen_embedding = eigen_embedding / np.sqrt(self.sampler.phase_order)
         return eigen_embedding
 
     def _test_eigenfunction(self) -> None:
@@ -134,10 +134,11 @@ class TestCompactLieGroups(unittest.TestCase):
         x_yinv = self.group.pairwise_diff(x, y)
         for eigenspace in self.group.lb_eigenspaces:
             f = eigenspace.basis_sum
-            dim_sq_f = f.representation.dimension ** 2
+            dim_sq_f = eigenspace.dimension ** 2
             cov1 = f(x_yinv).view(2, 2)/dim_sq_f
             embed_x, embed_y = self.embed(f, x), self.embed(f, y)
             cov2 = (embed_x @ torch.conj(embed_y.T))/dim_sq_f
+            print(cov1 - cov2)
             self.assertTrue(torch.allclose(cov1, cov2, atol=2e-1, rtol=2e-1))
             # print('passed')
 